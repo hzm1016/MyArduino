4f808e262dfe2cce8084a85a173c03dd4c78f589
Howard Z Marshall <hzm1016@earthlink.net>
2013-05-15 14:37:04 -0400

    Create dir Serial0, move Serial0.ino into it

--- Serial0/Serial0.ino
+++ Serial0/Serial0.ino
@@ -1,21 +1,5 @@
 /*
-  Software serial multple serial test
- 
- Receives from the hardware serial, sends to software serial.
- Receives from software serial, sends to hardware serial.
- 
- The circuit:
- * RX is digital pin 10 (connect to TX of other device)
- * TX is digital pin 11 (connect to RX of other device)
- 
- Note:
- Not all pins on the Mega and Mega 2560 support change interrupts,
- so only the following can be used for RX:
- 10, 11, 12, 13, 50, 51, 52, 53, 62, 63, 64, 65, 66, 67, 68, 69
- 
- Not all pins on the Leonardo support change interrupts,
- so only the following can be used for RX:
- 8, 9, 10, 11, 14 (MISO), 15 (SCK), 16 (MOSI).
+ Receives from the hardware Serial0, blinks LED, sends back to Serial0
  
  created back in the mists of time
  modified 25 May 2012
@@ -25,7 +9,7 @@
  This example code is in the public domain.
  
  */
-
+#define BAUD_RATE 9600
 int led = 13;
 int ledState = 0;
 int justGot = 0;
@@ -35,7 +19,7 @@ void setup()
   // initialize the LED digital pin as an output.
   pinMode(led, OUTPUT);     
   // Open serial communications and wait for port to open:
-  Serial.begin(115200);
+  Serial.begin(BAUD_RATE);
 
   delay(1000);  // wait for a second -- otherwise we send some garbage
 
